92ac54e8afccdfd9fcdd8377c1d225a742d32b87
Anders Bjerner <abjerner@skybrud.dk>
2013-11-12 13:38:31 +0100

    Initial commit to GitHub

---
+++ src/Skybrud.BorgerDk.Console/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Skybrud.BorgerDk.ConsoleApp {
+    
+    class Program {
+        
+        private static void Main(string[] args) {
+
+            BorgerDkService service = new BorgerDkService(BorgerDkEndpoint.Default, BorgerDkMunicipality.SlagelseKommune);
+
+            BorgerDkArticle art = service.GetArticleFromId(861);
+
+            Console.WriteLine(art.ToXElement(BorgerDkMunicipality.SlagelseKommune.Code, 1400));
+
+        }
+
+    }
+
+}
---
+++ src/Skybrud.BorgerDk.TestSite/Default.aspx.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Skybrud.BorgerDk.TestSite {
+
+    public partial class Default : System.Web.UI.Page {
+
+        protected void Page_Load(object sender, EventArgs e) {
+
+            BorgerDkService service = new BorgerDkService(BorgerDkEndpoint.Default, BorgerDkMunicipality.SlagelseKommune);
+
+            var art = service.GetArticleFromId(556);
+
+            Response.ContentType = "text/xml";
+            Response.Write(art.ToXElement(BorgerDkMunicipality.SlagelseKommune.Code, 1400));
+            Response.End();
+
+        }
+
+    }
+
+}
